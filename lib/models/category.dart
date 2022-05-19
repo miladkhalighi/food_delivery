00c83f9ffdbefaf62eb1a18eb2caa007ead7c0bc
Milad Khalighi <milad.kh4lighi@gmail.com>
2022-05-19 01:32:33 -0700

    add categoryItem model

---
+++ lib/models/category.dart
@@ -0,0 +1,21 @@
+class Category{
+  String name;
+  bool enabled;
+
+  Category({required this.name,this.enabled = false});
+}
+
+ List<Category> categoryList = [
+   Category(name: 'Foods'),
+   Category(name: 'Drinks'),
+   Category(name: 'Snacks'),
+   Category(name: 'Sauce'),
+
+   Category(name: 'Foods'),
+   Category(name: 'Drinks'),
+   Category(name: 'Snacks'),
+   Category(name: 'Sauce'),
+
+   //TODO : remove half of the list
+
+ ];
---
+++ lib/screens/get_start_screen/components/category_item.dart
@@ -0,0 +1,58 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants/colors.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final String name;
+  final Function() pressed;
+  final bool enabled;
+  final double width;
+
+  const CategoryItem({
+    Key? key,
+    required this.name,
+    required this.pressed,
+    this.enabled = false,
+    this.width = 60.0
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: pressed,
+      child: SizedBox(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(name,
+                style: enabled ? Theme.of(context).textTheme.bodyText1?.copyWith(color: SolidColors.primaryColor) :
+                Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.black.withOpacity(0.5)),
+              ),
+            ),
+            const SizedBox(height: 10,),
+            Container(
+              width: width,
+              height: 3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(1000),
+                color: enabled ? SolidColors.primaryColor : Colors.transparent,
+                boxShadow: enabled ?[
+                  const BoxShadow(
+                      color: SolidColors.primaryColor,
+                      offset: Offset(3, 3),
+                      blurRadius: 8
+                  )
+                ] : null
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
