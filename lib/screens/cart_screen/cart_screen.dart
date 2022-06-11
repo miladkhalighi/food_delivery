a154c7f94fcc07a9ba232d51191590808c2fafcd
miladkhalighi <milad.kh4lighi@gmail.com>
2022-06-11 04:37:28 -0700

    add slidable package in order to slide cart items

--- lib/screens/cart_screen/cart_screen.dart
+++ lib/screens/cart_screen/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_delivery/controllers/food_controller.dart';
 import 'package:food_delivery/models/item.dart';
@@ -81,85 +82,141 @@ class ItemCard extends StatelessWidget {
 
     var _controller = Get.find<FoodController>();
 
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-
-                SizedBox(
-                    width: 70,
-                    height: 70,
-                    child: CircleAvatar(backgroundImage: AssetImage(item.img),)),
-                const SizedBox(width: 16,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Slidable(
+      key: ValueKey(item.id),
+      endActionPane: ActionPane(
+          extentRatio: 0.4,
+          dismissible: DismissiblePane(
+              onDismissed: () {
+            Get.snackbar('dis','mis');
+          }),
+          motion: const DrawerMotion(),
+          children: [
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Text(item.name,style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 20),),
-                    const SizedBox(height: 8,),
-                    Text(item.price,style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 16,color: SolidColors.primaryColor),),
+                    CircleAvatar(
+                      child: IconButton(
+                        onPressed: (){},
+                        icon: const Icon(CupertinoIcons.heart,color: Colors.white,),
+                      ),
+                      backgroundColor: const Color(0xFFDF2C2C),
+                      radius: 24,
+                    ),
+                    const SizedBox(width: 8,),
+                    CircleAvatar(
+                      child: IconButton(
+                        onPressed: (){},
+                        icon: const Icon(CupertinoIcons.delete,color: Colors.white,),
+                      ),
+                      backgroundColor: const Color(0xFFDF2C2C),
+                      radius: 24,
+                    ),
                   ],
                 )
-              ],
+              ),
             ),
-          ),
-        ),
-        Positioned(
-          right: 16,
-          bottom: 16,
-          child: Container(
-
+            // const Flexible(child: SizedBox(width: 32,)),
+            // Flexible(
+            //   child: SizedBox(
+            //     width: 54,
+            //     height: 54,
+            //     child: Container(color: Colors.blue,)
+            //   ),
+            // ),
+            // const Flexible(child: SizedBox(width: 8,)),
+            // Flexible(
+            //   child: SizedBox(
+            //     width: 54,
+            //     height: 54,
+            //     child: Container(color: const Color(0xFFDF2C2C),),
+            //   ),
+            // ),
+          ]
+      ),
+      child: Stack(
+        children: [
+          Container(
             decoration: BoxDecoration(
-              color: SolidColors.primaryColor,
-              borderRadius: BorderRadius.circular(1000)
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
             ),
             child: Padding(
-              padding: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.all(16.0),
               child: Row(
                 children: [
-                  const SizedBox(width: 8,),
-                  Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                        splashColor: Colors.white.withOpacity(0.5),
-                        onTap:(){
-                          _controller.itemDecrement(item);
-                        },child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
-                      child: Text('-',style: TextStyle(color: Colors.white,fontSize: 20),),
-                    )),
-                  ),
-                  GetBuilder<FoodController>(
-                      id: 'quantity',
-                      builder: (GetxController controller) {
-                        return Text(item.quantity.toString(),style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.white));
-                      },
-                  ),
-                  Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.white.withOpacity(0.5),
-                        onTap:(){
-                      _controller.itemIncrement(item);
-                    },child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
-                      child: Text('+',style: TextStyle(color: Colors.white,fontSize: 20),),
-                    )),
-                  ),
-                  const SizedBox(width: 8,),
+
+                  SizedBox(
+                      width: 70,
+                      height: 70,
+                      child: CircleAvatar(backgroundImage: AssetImage(item.img),)),
+                  const SizedBox(width: 16,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(item.name,style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 20),),
+                      const SizedBox(height: 8,),
+                      Text(item.price,style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 16,color: SolidColors.primaryColor),),
+                    ],
+                  )
                 ],
               ),
             ),
           ),
-        )
-      ],
+          Positioned(
+            right: 16,
+            bottom: 16,
+            child: Container(
+
+              decoration: BoxDecoration(
+                color: SolidColors.primaryColor,
+                borderRadius: BorderRadius.circular(1000)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 8,),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                          splashColor: Colors.white.withOpacity(0.5),
+                          onTap:(){
+                            _controller.itemDecrement(item);
+                          },child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
+                        child: Text('-',style: TextStyle(color: Colors.white,fontSize: 20),),
+                      )),
+                    ),
+                    GetBuilder<FoodController>(
+                        id: 'quantity',
+                        builder: (GetxController controller) {
+                          return Text(item.quantity.toString(),style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.white));
+                        },
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        splashColor: Colors.white.withOpacity(0.5),
+                          onTap:(){
+                        _controller.itemIncrement(item);
+                      },child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0,vertical: 4),
+                        child: Text('+',style: TextStyle(color: Colors.white,fontSize: 20),),
+                      )),
+                    ),
+                    const SizedBox(width: 8,),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
