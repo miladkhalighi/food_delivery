be1b14a1d8c12c0f097e752508f7cd25f6465307
miladkhalighi <milad.kh4lighi@gmail.com>
2022-10-19 04:36:32 -0700

    add animations in food item widget

--- lib/screens/home_screen/components/food_item.dart
+++ lib/screens/home_screen/components/food_item.dart
@@ -1,40 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import '../../../constants/colors.dart';
 import '../../../models/item.dart';
 import '../../details_screen/ItemDetails.dart';
 
-class FoodItem extends StatelessWidget {
+class FoodItem extends StatefulWidget {
   final double width;
   final Item item;
 
   const FoodItem({
     Key? key,
-    this.width=150,
+    this.width = 150,
     required this.item,
   }) : super(key: key);
 
   @override
+  State<FoodItem> createState() => _FoodItemState();
+}
+
+class _FoodItemState extends State<FoodItem>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationCtr;
+  late Animation<double> _rotate;
+  late Animation<double> _fadeAndScale;
+
+  @override
+  void initState() {
+    _animationCtr = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _rotate = Tween<double>(begin: 0,end: 1.5).animate(CurvedAnimation(parent: _animationCtr, curve: const Interval(0, 0.7,curve: Curves.easeIn)));
+    _fadeAndScale = CurvedAnimation(parent: _animationCtr, curve: const Interval(0, 1,curve: Curves.easeIn),reverseCurve: Curves.easeOut);
+
+    _animationCtr.forward();
+    _animationCtr.addListener(() { });
+
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){
+      onTap: () {
         //Get.to(()=> ItemDetailsScreen(item: item));
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ItemDetailsScreen(item: item)));
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ItemDetailsScreen(item: widget.item)));
       },
       child: SizedBox(
-        width: width,
-        height: width * 1.55,
+        width: widget.width,
+        height: widget.width * 1.55,
         child: Stack(
           children: [
             Positioned(
-              top : width /4,
+              top: widget.width / 4,
               left: 0,
               right: 0,
               bottom: 0,
               child: Container(
-                height: width * 2 / 1.5,
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                height: widget.width * 2 / 1.5,
                 decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.all(Radius.circular(30)),
@@ -42,23 +68,40 @@ class FoodItem extends StatelessWidget {
                       BoxShadow(
                           color: Colors.grey,
                           offset: Offset(0, 0),
-                          blurRadius: 16
-                      )
-                    ]
-                ),
-                child: Column(
-                  children: [
-                    const Spacer(),
-                    Text(
-                      item.name,
-                      style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 26),
-                      maxLines: 2,
-                      textAlign: TextAlign.center,
+                          blurRadius: 16)
+                    ]),
+                child: ScaleTransition(
+                  scale: _fadeAndScale,
+                  child: FadeTransition(
+                    opacity: _fadeAndScale,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          widget.item.name,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1
+                              ?.copyWith(fontSize: 22),
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: widget.width * 0.1,
+                        ),
+                        Text(
+                          widget.item.price,
+                          style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                              fontSize: 18, color: SolidColors.primaryColor),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: widget.width * 0.15,
+                        )
+                      ],
                     ),
-                    SizedBox(height: width * 0.1,),
-                    Text(item.price,style: Theme.of(context).textTheme.bodyText1?.copyWith(fontSize: 20,color: SolidColors.primaryColor),),
-                    SizedBox(height: width * 0.15,)
-                  ],
+                  ),
                 ),
               ),
             ),
@@ -68,30 +111,35 @@ class FoodItem extends StatelessWidget {
                 left: 0,
                 right: 0,
                 child: Hero(
-                  tag: item.id,
-                  child: Container(
-                    width: width - width * 0.15,
-                    height: width - width * 0.15,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage(item.img),
-                            fit: BoxFit.cover
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 32,
-                              offset: const Offset(0, 16)
-                          )
-                        ]
+                  tag: widget.item.id,
+                  child: RotationTransition(
+                    turns: _rotate,
+                    child: Container(
+                      width: widget.width - widget.width * 0.15,
+                      height: widget.width - widget.width * 0.15,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage(widget.item.img),
+                              fit: BoxFit.cover),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                blurRadius: 16,
+                                offset: const Offset(0, 0))
+                          ]),
                     ),
                   ),
-                )
-            )
+                ))
           ],
-        ),
+        )
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _animationCtr.dispose();
+    super.dispose();
   }
 }
